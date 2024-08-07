6ffacfac78c907e37cd98a199137551bc9733b01
Christopher Marx <dev@christopher-marx.de>
2024-08-07 10:49:20 +0200

    update

--- lib/src/fluit.dart
+++ lib/src/fluit.dart
@@ -1,5 +1,6 @@
 import 'package:fluit/src/design_system/custom_theme.dart';
 import 'package:fluit/src/studio/features/controls/play_controls_wrapper.dart';
+import 'package:fluit/src/studio/features/examples/animation_subjects.dart';
 import 'package:fluit/src/studio/studio_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:state_beacon/state_beacon.dart';
@@ -11,7 +12,11 @@ Future<void> initFluit() async {
 class FluitStudio extends StatefulWidget {
   const FluitStudio({
     super.key,
+    this.subjects,
   });
+
+  /// A map of example widgets that can be used as subjects for animations.
+  final Map<String, Widget Function(BuildContext)>? subjects;
 
   @override
   State<FluitStudio> createState() => _FluitStudioState();
@@ -21,8 +26,8 @@ class _FluitStudioState extends State<FluitStudio> {
   @override
   void initState() {
     super.initState();
-    BeaconObserver.instance =
-        LoggingObserver(); // or BeaconObserver.useLogging()
+    BeaconObserver.instance = LoggingObserver();
+    subjectBuildersBeacon.value = widget.subjects ?? premadeExamples;
   }
 
   @override
